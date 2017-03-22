37051da2b77231c9f9a2d3db35d9bc6b9a71cf14
Owen van Dijk <owen.vandijk@firstborn.com>
2017-03-22 18:44:54 -0400

    updating java class

--- android/src/main/java/com/almouro/reactnativeflashlight/ReactNativeFlashlightModule.java
+++ android/src/main/java/com/almouro/reactnativeflashlight/ReactNativeFlashlightModule.java
@@ -3,18 +3,18 @@ package com.almouro.reactnativeflashlight;
 import android.content.Context;
 import android.util.AndroidException;
 
+import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
 class ReactNativeFlashlightModule extends ReactContextBaseJavaModule {
-    private Context context;
     private FlashlightControl flashlight;
 
     public ReactNativeFlashlightModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.context = reactContext;
         flashlight = new FlashlightControl();
     }
 
@@ -28,9 +28,13 @@ class ReactNativeFlashlightModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void toggleFlashlight(Callback onSuccess, Callback onFailure) {
+    public void turnFlashlight(Boolean on, Callback onSuccess, Callback onFailure) {
         try {
-            flashlight.toggle();
+            if (on) {
+              flashlight.turnOn();
+            } else {
+              flashlight.turnOff();
+            }
         } catch(AndroidException e) {
             onFailure.invoke(e.getMessage());
             return;
